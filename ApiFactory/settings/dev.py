8fc99c864b50a420c0dcdabc2de24cddc85adbd7
sidney31 <surovbogdan05@gmail.com>
2024-01-18 11:55:22 +0300

    initial commit

---
+++ ApiFactory/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-!jfwu29rc-foc77+lfof)tyi!+#f*x@j%)-ejl0v+4umk-s0gh"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
