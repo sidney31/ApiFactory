bd82cbe70f2a24916880f0b00adffab1f0bd1a68
sidney31 <surovbogdan05@gmail.com>
2024-01-24 19:25:35 +0300

    create base template; install bootstrap5

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.models import Page
 
-# Create your models here.
+
+class MainPage(Page):
+    text = models.CharField(max_length=256)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('text')
+    ]
