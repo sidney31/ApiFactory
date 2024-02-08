0c03388a46ce6cce51400d33612c54020f715ca6
sidney31 <surovbogdan05@gmail.com>
2024-02-08 15:12:36 +0300

    add response speed for `marquee`

--- ApiFactory/static/js/index.js
+++ ApiFactory/static/js/index.js
@@ -1,58 +1,62 @@
 $(document).ready(function () {
-    let lastScrollTop = 0;
+	let lastScrollTop = 0;
 
-    $(window).scroll(() => {
-        if ($(this).scrollTop() > lastScrollTop + 10) {
-            $('.navbar').css('transform', 'translateY(-100px)')
-        } else if ($(this).scrollTop() < lastScrollTop - 10) {
-            $('.navbar').css('transform', 'translateY(0)')
-        }
-        lastScrollTop = $(this).scrollTop();
-    });
+	$(window).scroll(() => {
+		if ($(this).scrollTop() > lastScrollTop + 10) {
+			$(".navbar").css("transform", "translateY(-100px)");
+		} else if ($(this).scrollTop() < lastScrollTop - 10) {
+			$(".navbar").css("transform", "translateY(0)");
+		}
+		lastScrollTop = $(this).scrollTop();
+	});
 
-    const carousel = new bootstrap.Carousel($('#carousel'), {
-        interval: 2000,
-    })
+	const carousel = new bootstrap.Carousel($("#carousel"), {
+		interval: 2000,
+	});
 
-    const $mq = $('#marquee').marquee({
-        duration: 25000,
-        gap: 10,
-        delayBeforeStart: 0,
-        direction: 'left',
-        duplicated: true,
-        // pauseOnHover: true,
-        startVisible: true,
-    });
+	let $mq = $("#marquee").marquee({
+		duration: window.matchMedia("(max-width: 1200px)").matches ? 5000 : 25000,
+		gap: 0,
+		delayBeforeStart: 0,
+		direction: "left",
+		duplicated: true,
+		// pauseOnHover: true,
+		startVisible: true,
+	});
 
-    const $swithes = $('[id^=switch]')
+	const $swithes = $("[id^=switch]");
 
-    $swithes.on('change', function () {
-        const $root = $(this).closest('.section2');
-        $root.children('.content').css(
-            'opacity', ($(this).is(':checked') ? '100%' : '30%'),
-            'pointer-events', ($(this).is(':checked') ? 'auto' : 'none')
-        );
-        // $root.children('.content').css('pointer-events', ($(this).is(':checked') ? 'auto' : 'none'));
-    });
+	$swithes.on("change", function () {
+		const $root = $(this).closest(".section2");
+		$root
+			.children(".content")
+			.css(
+				"opacity",
+				$(this).is(":checked") ? "100%" : "30%",
+				"pointer-events",
+				$(this).is(":checked") ? "auto" : "none"
+			);
+		// $root.children('.content').css('pointer-events', ($(this).is(':checked') ? 'auto' : 'none'));
+	});
 
+	const myOffcanvas = document.querySelector("#offcanvas");
+	const navLinks = document.querySelectorAll(".nav-link");
+	const offcanvas = new bootstrap.Offcanvas(myOffcanvas);
 
-    const myOffcanvas = document.querySelector('#offcanvas')
-    const navLinks = document.querySelectorAll('.nav-link')
-    const offcanvas = new bootstrap.Offcanvas(myOffcanvas)
+	navLinks.forEach((e) => {
+		e.addEventListener("click", () => {
+			offcanvas.hide();
+		});
+	});
 
-    navLinks.forEach(e => {
-        e.addEventListener('click', () => {
-            offcanvas.hide();
-        })
-    })
+	myOffcanvas.addEventListener("show.bs.offcanvas", () => {
+		myOffcanvas.setAttribute(
+			"style",
+			"max-height: calc(100vh - 100px)!important;"
+		);
+	});
 
-    myOffcanvas.addEventListener('show.bs.offcanvas', () => {
-        myOffcanvas.setAttribute("style", "max-height: calc(100vh - 100px)!important;");
-    })
-
-    myOffcanvas.addEventListener('hide.bs.offcanvas', () => {
-        myOffcanvas.setAttribute("style", "max-height: 0!important;");
-    })
-
-})
-
+	myOffcanvas.addEventListener("hide.bs.offcanvas", () => {
+		myOffcanvas.setAttribute("style", "max-height: 0!important;");
+	});
+});
