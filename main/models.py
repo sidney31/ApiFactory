6091139adcd1658a6d2ada96187d74cc22d00642
sidney31 <surovbogdan05@gmail.com>
2024-02-16 12:02:38 +0300

    Create model for `callback`; Replace `form.errors` widnow; Add types for `alert`;

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
 
+from phonenumber_field.modelfields import PhoneNumberField
 
 class MainPage(Page):
     text = models.CharField(max_length=256)
@@ -9,3 +10,11 @@ class MainPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('text')
     ]
+
+class CallbackApplication(models.Model):
+    name = models.CharField(blank=False, null=False, max_length=100)
+    phone = PhoneNumberField(blank=False, null=False)
+    agreement = models.BooleanField(blank=False, null=False)
+    
+    def __str__(self):
+        return self.name
