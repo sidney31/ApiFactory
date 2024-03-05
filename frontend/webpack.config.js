9c405d13011267942090de62d32ff2e399e19713
sidney31 <allonsosamp@gmail.com>
2024-03-05 08:56:37 -0500

    Base configuration webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,69 +1,43 @@
-'use strict'
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
 
-const path = require("path");
-const autoprefixer = require('autoprefixer')
-const webpack = require("webpack");
+module.exports = (env) => {
+  return {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[contenthash].js',
+      clean: true,
+        publicPath: '/',
+    },
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "./static/js"),
-    filename: "bundle.js",
-  },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            "style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
 
-  resolve: {
-    extensions: ['.js'],
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  autoprefixer
-                ]
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
-      },
-
-      {
-        test: /\.(css)$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery'
-    }),
-
-  ],
-
-  optimization: {
-    minimize: true,
-  },
-
-};
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'templates', "base.html"),
+        publicPath: '/build/'
+      }),
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+        'bootstrap': 'bootstrap',
+      }),
+    ]
+  }
+}
