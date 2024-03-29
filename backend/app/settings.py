b294f04a451fcfe3455496828ac346ded04f902c
sidney31 <allonsosamp@gmail.com>
2024-03-29 03:22:26 -0400

    force

---
+++ backend/app/settings.py
@@ -0,0 +1,127 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
+
+SECRET_KEY = 'django-insecure-rgi=s4p$3ti3g(+&aas5%)4e1!v^6(b0_#lmrp+omkao&8^zlk'
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'corsheaders',
+    'rest_framework',
+    'mozilla_django_oidc',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+ROOT_URLCONF = 'app.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'app.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'mozilla_django_oidc.contrib.drf.OIDCAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+AUTHENTICATION_BACKENDS = (
+    'mozilla_django_oidc.auth.OIDCAuthenticationBackend',
+)
+
+OIDC_RP_CLIENT_ID = 'web'
+OIDC_RP_CLIENT_SECRET = 'F5fWCQAvCVkBbucLM2MZXw5z4DdewmL8'
+OIDC_OP_AUTHORIZATION_ENDPOINT = 'https://id.api-factory.ru:8443/realms/API-FACTORY/protocol/openid-connect/auth'
+OIDC_OP_TOKEN_ENDPOINT = 'https://id.api-factory.ru:8443/realms/API-FACTORY/protocol/openid-connect/token'
+OIDC_OP_USER_ENDPOINT = 'https://id.api-factory.ru:8443/realms/API-FACTORY/protocol/openid-connect/userinfo'
+OIDC_OP_JWKS_ENDPOINT = 'https://id.api-factory.ru:8443/realms/API-FACTORY/protocol/openid-connect/certs'
+OIDC_RP_SIGN_ALGO = 'RS256'
+OIDC_VERIFY_SSL = False
+
+LOGIN_URL = 'oidc_authentication_init'
+LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/account'
+
+CSRF_TRUSTED_ORIGINS = ['https://web.api-factory.ru']
+
+# CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOWED_ORIGINS = ['https://web.api-factory.ru']
+
+CORS_ORIGIN_ALLOW_ALL = True
---
+++ frontend/src/main.tsx
@@ -0,0 +1,39 @@
+// @ts-nocheck
+
+import axios from 'axios'
+import 'normalize.css'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Account } from './screens/Account/Account.tsx'
+import { ErrorPage } from './screens/ErrorPage.tsx'
+import Home from './screens/Home/Home.tsx'
+import UserService from './services/UserService.js'
+import './styles/common.scss'
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: 'account/',
+		element: <Account />
+	},
+])
+
+const _axios = axios.create();
+_axios.interceptors.request.use((config) => {
+  if (UserService.isLoggedIn()) {
+    const cb = () => {
+      config.headers.Authorization = `Bearer ${UserService.getToken()}`;
+      return Promise.resolve(config);
+    };
+    return UserService.updateToken(cb);
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<RouterProvider router={router} />
+)
+UserService.initKeycloak();
