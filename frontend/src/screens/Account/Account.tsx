596a96d36d32e5042babdc20eaff2bf957d623c0
sidney31 <allonsosamp@gmail.com>
2024-04-16 05:37:15 -0400

    Add service blocks in account page

--- frontend/src/screens/Account/Account.tsx
+++ frontend/src/screens/Account/Account.tsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 import { Navbar } from '../../components/navbar/Navbar.tsx'
 import '../../styles/common.scss'
 
+import ServiceCard from '../../components/serviceCard/ServiceCard.tsx'
+import styles from './Account.module.scss'
+import { services_db } from './services_db.js'
+
 export const Account = () => {
 	const { keycloak } = useKeycloak()
 
@@ -33,7 +37,7 @@ export const Account = () => {
 						}
 					/>
 					<main className='relative top-[100px]'>
-						<div className='container'>
+						{/* <div className='container'>
 							<h2 className='font-semibold text-[36px] mb-[36px]'>Сервисы</h2>
 							<p>{keycloak.tokenParsed?.name}</p>
 							<p>{keycloak.tokenParsed?.email}</p>
@@ -49,9 +53,23 @@ export const Account = () => {
 									Выйти
 								</button>
 							)}
+						</div> */}
+
+						<div className='container'>
+							<h2 className='font-semibold text-[36px] mb-[36px]'>Сервисы</h2>
+							<div className={styles.cards_wrapper}>
+								{services_db.map((service, index) => (
+									<ServiceCard
+										key={index}
+										title={service.hero_title}
+										titleBackground={service.hero_image}
+										name={service.name}
+										logo={service.logo}
+									/>
+								))}
+							</div>
 						</div>
 					</main>
-					)
 				</>
 			)}
 		</>
---
+++ frontend/src/screens/Account/services_db.js
@@ -0,0 +1,38 @@
+export const services_db = [
+	{
+		hero_title: 'МОНИТОРИНГ СТАТИСТИКА УВЕДОМЛЕНИЯ СЕТИ',
+		hero_image: '/serviceBackground.png',
+		name: 'EyeFactory',
+		logo: '/',
+	},
+	{
+		hero_title: 'УЧЕТ КОНФИГУРАЦИЯ ОБНАРУЖЕНИЕ СЕТИ',
+		hero_image: '/serviceBackground.png',
+		name: 'AiCensys',
+		logo: '/',
+	},
+	{
+		hero_title: 'УЧЕТ КОНФИГУРАЦИЯ КОМПЬЮТЕРЫ СОФТ',
+		hero_image: '/serviceBackground.png',
+		name: 'Inventory',
+		logo: '/',
+	},
+	{
+		hero_title: 'ХРАНИЛИЩЕ ШИФРОВАНИЕ ДАННЫЕ БЕЗОПАСНОСТЬ',
+		hero_image: '/serviceBackground.png',
+		name: 'DataRoom',
+		logo: '/',
+	},
+	{
+		hero_title: 'ХРАНИЛИЩЕ ДАННЫЕ КОЛЛАБОРАЦИЯ СИНХРОНИЗАЦИЯ',
+		hero_image: '/serviceBackground.png',
+		name: 'Cloud',
+		logo: '/',
+	},
+	{
+		hero_title: 'СЧЕТА СКИДКИ ОПЛАТА АВТОМАТИЗАЦИЯ',
+		hero_image: '/serviceBackground.png',
+		name: 'Биллинг',
+		logo: '/',
+	},
+]
