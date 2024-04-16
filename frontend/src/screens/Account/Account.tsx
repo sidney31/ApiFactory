2001387effa62854887c24ba187511cc84a33c1d
sidney31 <allonsosamp@gmail.com>
2024-04-16 11:09:59 +0300

    Create variant of account page for unauitharization users

--- frontend/src/screens/Account/Account.tsx
+++ frontend/src/screens/Account/Account.tsx
@@ -5,36 +5,55 @@ import '../../styles/common.scss'
 
 export const Account = () => {
 	const { keycloak } = useKeycloak()
+
 	return (
 		<>
-			<Navbar
-				nav_links={new Array()}
-				logo_path='/svg/logo.svg'
-				header_action={
-					<>
-						<Link to='/profile'>
-							<button>Профиль</button>
-						</Link>
-					</>
-				}
-			/>
-			<main className='relative top-[100px]'>
-				<div className='container'>
-					<h2 className='font-semibold text-[36px] mb-[36px]'>Сервисы</h2>
-					<p>{keycloak.tokenParsed?.name}</p>
-					<p>{keycloak.tokenParsed?.email}</p>
-					{!!keycloak.authenticated && (
-						<button
-							type='button'
-							onClick={() =>
-								keycloak.logout({ redirectUri: 'https://web.api-factory.ru/' })
-							}
-						>
-							Выйти
-						</button>
-					)}
+			{(!keycloak.authenticated && (
+				<div className='h-[100svh] flex flex-col items-center justify-center'>
+					<h1>Требуется авторизация</h1>
+					<button
+						onClick={() => {
+							keycloak.login()
+						}}
+					>
+						Авторизация
+					</button>
 				</div>
-			</main>
+			)) || (
+				<>
+					<Navbar
+						nav_links={new Array()}
+						logo_path='/svg/logo.svg'
+						header_action={
+							<>
+								<Link to='/profile'>
+									<button>Профиль</button>
+								</Link>
+							</>
+						}
+					/>
+					<main className='relative top-[100px]'>
+						<div className='container'>
+							<h2 className='font-semibold text-[36px] mb-[36px]'>Сервисы</h2>
+							<p>{keycloak.tokenParsed?.name}</p>
+							<p>{keycloak.tokenParsed?.email}</p>
+							{!!keycloak.authenticated && (
+								<button
+									type='button'
+									onClick={() =>
+										keycloak.logout({
+											redirectUri: 'https://web.api-factory.ru/',
+										})
+									}
+								>
+									Выйти
+								</button>
+							)}
+						</div>
+					</main>
+					)
+				</>
+			)}
 		</>
 	)
 }
