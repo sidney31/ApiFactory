35dae468830a5b216e47c6f9e7752d4db4d4c5e2
sidney31 <allonsosamp@gmail.com>
2024-04-17 17:49:33 +0300

    Create form handler for feedback

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,7 +1,9 @@
+import { useFormik } from 'formik'
 import PhoneInput from 'react-phone-number-input'
 import ru from 'react-phone-number-input/locale/ru'
 import 'react-phone-number-input/style.css'
 import { Link } from 'react-router-dom'
+import * as Yup from 'yup'
 import styles from './Footer.module.scss'
 
 interface Props {
@@ -9,13 +11,24 @@ interface Props {
 }
 
 export const Footer = (props: Props) => {
-	// const NAV_LINKS = [
-	// 	{ title: 'О компании', url: '/about' },
-	// 	{ title: 'Отраслевые решения', url: '/solutions' },
-	// 	{ title: 'Услуги и цены', url: '/services_and_prices' },
-	// 	{ title: 'Заказчики', url: '/customers' },
-	// 	{ title: 'Контакты', url: '/contacts' },
-	// ]
+	const { handleSubmit, setValues, values, handleChange } = useFormik({
+		initialValues: {
+			name: '',
+			phone: '',
+			question: '',
+			agreement: false,
+		},
+		validationSchema: Yup.object({
+			name: Yup.string().required(),
+			phone: Yup.string().required(),
+			question: Yup.string().required(),
+			agreement: Yup.boolean().required(),
+		}),
+		onSubmit: values => {
+			console.log(values)
+		},
+	})
+
 	return (
 		<footer ref={props.reference} className={`${styles.footer}`}>
 			<div className='container'>
@@ -23,27 +36,49 @@ export const Footer = (props: Props) => {
 					<h2 data-aos='fade-up-right' className={styles.footer_formTitle}>
 						Отправьте заявление, и с Вами обязательно свяжутся
 					</h2>
-					<form data-aos='fade-up-left' id='feedback_form'>
+					<form
+						noValidate
+						onSubmit={handleSubmit}
+						data-aos='fade-up-left'
+						id='feedback_form'
+					>
 						<div className={styles.footer_form_field}>
-							<input type='text' placeholder='Имя' />
+							<input
+								id='name'
+								onChange={handleChange}
+								value={values.name}
+								type='text'
+								placeholder='Имя'
+							/>
 						</div>
 						<div className={styles.footer_form_field}>
 							<PhoneInput
+								id='phone'
+								value={values.phone}
+								onChange={(phone: string) =>
+									setValues({
+										...values,
+										phone: phone,
+									})
+								}
 								className={styles.footer_phoneInput}
 								labels={ru}
 								international
 								defaultCountry='RU'
-								countryCallingCodeEditable={false}
-								onChange={() => {}}
 							/>
 						</div>
 						<div className={styles.footer_form_field}>
-							<input type='text' placeholder='Вопрос' />
+							<input
+								id='question'
+								onChange={handleChange}
+								value={values.question}
+								placeholder='Вопрос'
+							/>
 						</div>
 						<div
 							className={`${styles.footer_form_field} flex flex-row gap-[1em]`}
 						>
-							<input id='agreement' type='checkbox' />
+							<input id='agreement' onChange={handleChange} type='checkbox' />
 							<label className={styles.footer_form_label} htmlFor='agreement'>
 								Нажимая кнопку «Отправить», я даю свое согласие на обработку
 								моих персональных данных, в соответствии с Федеральным законом
